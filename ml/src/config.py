6bbaca704b2fcbd750cabb6897e959fedf439122
oscarigrexas <oscarigrexas@gmail.com>
2024-11-14 19:23:00 +0100

    feat: Start defining ML data access patterns

--- ml/src/config.py
+++ ml/src/config.py
@@ -1,13 +1,8 @@
-import os
+from omegaconf import OmegaConf
 
 import mlflow
-from omegaconf import OmegaConf
 
 config = OmegaConf.load("conf/config.yml")
 
 mlflow.set_tracking_uri(config.mlflow_uri)
 mlflow.set_experiment(config.experiment_name)
-
-os.environ["AWS_ACCESS_KEY_ID"] = config.minio_user
-os.environ["AWS_SECRET_ACCESS_KEY"] = config.minio_password
-os.environ["MLFLOW_S3_ENDPOINT_URL"] = config.minio_uri
